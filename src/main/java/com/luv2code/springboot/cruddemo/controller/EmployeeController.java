96b833da680296877a081d0a120a8ae423016b39
rupi <varinderjit17@gmail.com>
2024-09-22 11:42:07 +1200

    Add/Save employee working

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,5 +33,18 @@ public class EmployeeController {
         theModel.addAttribute("employees", theEmployees);
 
         return "list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
